6b677b6ffc8df7a8708f1aae8c06af2a4f5a0b39
Helpingakp <99520904+Helpingakp@users.noreply.github.com>
2023-04-11 08:07:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Team-Alisha/Alisha",
+    "https://github.com/Helpingakp/Alisha",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
